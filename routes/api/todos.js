479e3ee31ce58e07668460d2bbb7d09da21ebfe4
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-08 14:24:35 -0400

    Back-end DELETE route.

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -7,7 +7,7 @@ const Todo = require('../../models/Todo');
 // @desc   Get All Todos
 // @access Public
 router.get('/', (req, res) => {
-  // Get items and sort
+  // Get todos and sort
   Todo.find()
     .sort({ date: -1 })
     .then((todos) => res.json(todos));
@@ -22,6 +22,22 @@ router.post('/', (req, res) => {
     date: req.body.date,
   });
   newTodo.save().then((todo) => res.json(todo));
+});
+
+// @route  DELETE api/todos
+// @desc   Delete A Todo
+// @access Public
+router.delete('/:id', (req, res) => {
+  // Find todo to delete by ID
+  Todo.findById(req.params.id)
+    // Remove todo from DB
+    .then((todo) =>
+      todo
+        .remove()
+        // Send Success message if success || Not Found if failed
+        .then(() => res.json({ success: true }))
+    )
+    .catch((err) => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
